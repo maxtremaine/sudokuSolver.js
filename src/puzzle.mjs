1c911a42e196400d2f4cd3a3fb8f752889b0f5f4
maxtremaine <max.tremaine@gmail.com>
2021-04-09 00:38:49 -0400

    Pulls in and validates a puzzle.

--- src/puzzle.mjs
+++ src/puzzle.mjs
@@ -1,21 +1,88 @@
 import { readFile } from 'fs/promises';
 
+// Load Rules
+
 const {
 	emptyGrid,
 	groups,
 	fileToStringConversionIndexes
 } = JSON.parse(await readFile('./src/puzzleRules.json'));
 
+// Factories
+
+const Cell = ({index, value, sudokuString}) => {
+	const relatedValues = getRelatedNumbers(sudokuString)(index);
+	return {
+		index,
+		value,
+		relatedCells: getRelatedCells(index),
+		relatedValues,
+		possibleValues: missingDigits(relatedValues)
+	};
+};
+
+
 // Validation
 
 const validateSudokuFile = sudokuFile => {
 	const pattern = /^(\s|\n|\||_|-|[1-9]|[a-i]){167}$/
-	return sudokuFile.match(pattern) === sudokuFile
+	return sudokuFile.match(pattern)[0] === sudokuFile
 }
 
 const validateSudokuString = sudokuString => {
 	const pattern = /^(_|[1-9]){81}$/
-	return sudokuString.match(pattern) === sudokuString;
+	return sudokuString.match(pattern)[0] === sudokuString;
 }
 
-export { validateSudokuFile, validateSudokuString };
+// Puzzle Actions
+
+const sudokuFileToString = sudokuFile => {
+	const characterList = fileToStringConversionIndexes.map(index => sudokuFile[index]);
+	return characterList.join('');
+}
+
+const getIndexedValues = sudokuString => indexes => (
+	indexes.map(index => sudokuString[index])
+);
+
+const missingDigits = values => {
+	const digits = [...Array(10).keys()].slice(1);
+	return digits.filter(digit => !values.includes(digit));
+};
+
+const validateGroup = groupString => {
+	if(groupString.length !== 9) return false;
+	const numbers = Array.from(groupString).filter(number => number !== '_');
+	const numberSet = new Set(numbers);
+	return numbers.length === numberSet.size;
+};
+
+const getRelatedCells = cellIndex => {
+	const relatedGroups = groups.filter(group => group.includes(cellIndex));
+	return relatedGroups.flat();
+};
+
+const getRelatedNumbers = sudokuString => cellIndexes => {
+	const cellValues = cellIndexes.map(index => sudokuString[index]);
+	const cellNumbers = cellValues.filter(value => value !== '_');
+	return cellNumbers;
+};
+
+// Composites
+
+const validatePuzzle = sudokuString => {
+	const groupValues = groups.map(group => getIndexedValues(sudokuString)(group));
+	const groupStrings = groupValues.map(values => values.join(''));
+	const groupValidities = groupStrings.map(groupString => validateGroup(groupString));
+	const invalidResults = groupValidities.filter(validity => validity === false);
+	return invalidResults.length === 0
+}
+
+const cellDegreesOfFreedom = index => sudokuString => {
+	const relatedCells = getRelatedCells(index);
+	const relatedValues = getIndexedValues(sudokuString)(relatedCells);
+	const underscores = relatedValues.filter(values => values === '_');
+	return underscores.length;
+}
+
+export { Cell, sudokuFileToString, validateSudokuFile, validateSudokuString, validatePuzzle };
--- src/run.mjs
+++ src/run.mjs
@@ -1,5 +1,15 @@
-import { pipe } from './components.mjs'
+import { Cell, sudokuFileToString, validateSudokuFile,
+	validateSudokuString, validatePuzzle } from './puzzle.mjs';
+import { changeCharacter, countCharacterInString } from './components.mjs';
+import { readFile } from 'fs/promises';
 
-const addOne = x => x + 1;
+const startPuzzle = await readFile('./io/start.sudoku', 'utf-8');
 
-console.log(addOne(1))
+if(!validateSudokuFile(startPuzzle)) throw 'This is not a valid sudoku file.';
+
+const sudokuString = sudokuFileToString(startPuzzle);
+
+if(!validateSudokuString(sudokuString)) throw 'This is not a valid sudoku string.';
+if(!validatePuzzle(sudokuString)) throw 'This is not a valid puzzle.';
+
+console.log(sudokuString);
