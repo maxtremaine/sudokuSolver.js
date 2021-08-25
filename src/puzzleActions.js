b56085284a251eae1e4119eb41ecc3cd61e46fd0
maxtremaine <max.tremaine@gmail.com>
2021-08-25 17:53:11 -0400

    Added conversion functions.

--- src/puzzleActions.js
+++ src/puzzleActions.js
@@ -1,1 +1,18 @@
 const { emptyGrid, groups, fileToStringConversionIndexes } = require('./puzzleData').puzzleData
+const { replaceSubstring } = require('./pureFunctions')
+
+const sudokuFileToString = sudokuFile => {
+    return fileToStringConversionIndexes
+        .map(index => sudokuFile[index])
+        .join('')
+}
+
+const sudokuStringToFile = sudokuString =>  {
+    let emptyPuzzle = emptyGrid.join('')
+    fileToStringConversionIndexes.forEach(( index, n) => {
+        emptyPuzzle = replaceSubstring({ index, substring: sudokuString[n]})(emptyPuzzle)
+    })
+    return emptyPuzzle
+}
+
+module.exports = { sudokuFileToString, sudokuStringToFile }
--- test/puzzleActions.test.js
+++ test/puzzleActions.test.js
@@ -1,1 +1,27 @@
-const {  } = require('assert')
+const { deepStrictEqual } = require('assert')
+const { sudokuFileToString, sudokuStringToFile } = require('../src/puzzleActions')
+
+describe('Puzzle Actions', () => {
+    const fileString = [
+        '  abc def ghi',
+        '1 7__|_4_|__1',
+        '2 __1|___|2__',
+        '3 _6_|2_9|_8_',
+        '  -----------',
+        '4 __3|5_4|9__',
+        '5 1__|___|__4',
+        '6 __2|1_8|5__',
+        '  -----------',
+        '7 _1_|9_6|_7_',
+        '8 __8|___|4__',
+        '9 6__|_2_|__8'].join('\n')
+    const sudokuString = '7___4___1__1___2___6_2_9_8___35_49__1_______4__21_85___1_9_6_7___8___4__6___2___8'
+    
+    it('Should convert a .sudoku file to a sudoku string.', () => {
+        deepStrictEqual(sudokuFileToString(fileString), sudokuString)
+    })
+
+    it('Should convert a sudoku string to a .sudoku file.', () => {
+        deepStrictEqual(sudokuStringToFile(sudokuString), fileString)
+    })
+})
