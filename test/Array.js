15524e9de4a0df3dd4cf0d788b0c9417aeeb809f
Max Tremaine <max.tremaine@gmail.com>
2022-12-31 18:32:01 -0500

    Cleaned up testing.

--- test/Array.js
+++ test/Array.js
@@ -1,9 +1,9 @@
-const assert = require('assert').deepStrictEqual
+const { deepStrictEqual } = require('assert')
 require('../src/Array')
 
 describe('Added Array Methods', () => {
 	it('Should return missing digits from an array', () => {
-		assert([ 1, 2, 3 ].getMissingDigits(), [ 4, 5, 6, 7, 8, 9 ])
-		assert([ 10, 5, 'hi', false ].getMissingDigits(), [ 1, 2, 3, 4, 6, 7, 8, 9 ])
+		deepStrictEqual([ 1, 2, 3 ].getMissingDigits(), [ 4, 5, 6, 7, 8, 9 ])
+		deepStrictEqual([ 10, 5, 'hi', false ].getMissingDigits(), [ 1, 2, 3, 4, 6, 7, 8, 9 ])
 	})
 })
