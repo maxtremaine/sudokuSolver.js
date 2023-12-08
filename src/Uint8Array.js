21866c8acac4632ffbb0d07cc4181cd9611342f9
Max Tremaine <me@maxtremaine.com>
2023-12-07 21:47:26 -0500

    Moved Sudoku functionality into Uint8Array'

--- src/Uint8Array.js
+++ src/Uint8Array.js
@@ -1,3 +1,30 @@
+const BlankCell = require('./BlankCell')
+
+Uint8Array.fromSudokuFile = (sudokuFile) => {
+	if(typeof sudokuFile !== 'string') {
+		return [ 'The \'sudokuFile\' parameter must be a string.', Uint8Array.from([]) ]
+	}
+
+	if(sudokuFile.length !== 167) {
+		return [ 'The \'sudokuFile\' parameter must have a length of 167.', Uint8Array.from([]) ]
+	}
+
+	for(const character of sudokuFile) {
+		if(!sudokuFileValues.includes(character)) {
+			return [ 'You cannot use \'' + character + '\' in the \'sudokuFile\' parameter.', Uint8Array.from([]) ]
+		}
+	}
+
+	return ['', Uint8Array.from(fileToStringConversionIndexes.map(x => sudokuFile[x])
+		.map(x => {
+			if (x === '_') {
+				return 0
+			} else {
+				return Number(x)
+			}
+		}))]
+}
+
 Uint8Array.prototype.getMissingDigits = function() {
 	return Uint8Array.from([...Array(10).keys()].slice(1) // Range from 1 to 9, inclusive.
 		.filter(x => !this.includes(x)))
@@ -23,3 +50,115 @@ Uint8Array.prototype.unique = function() {
 		return acc
 	}, []))
 }
+
+Uint8Array.prototype.validateSudokuPuzzle = function() {
+	if(this.length !== 81) return 'Sudoku puzzles must have a length of 81.'
+
+	for(const cell of this) {
+		if(cell < 0 || cell > 9) return 'Cells must be between 0 and 9, inclusive.'
+	}
+
+	for(const group of groups) {
+		const groupCells = group.map(x => this[x])
+			.filter(x => x !== 0)
+
+		if(Uint8Array.from(groupCells).hasDuplicates()) {
+			return 'The sudoku file is not valid because a group as two of the same value.'
+		}
+	}
+
+	return ''
+}
+
+Uint8Array.prototype.getRelatedCells = function(index) {
+	return Uint8Array.from(groups.filter(group => group.includes(index))
+		.flat())
+		.unique() // Remove repeating indexes.
+		.map(i => this[i])
+		.filter(x => x !== 0)
+		.unique() // Remove repeating values.
+		.sort()
+}
+
+Uint8Array.prototype.getBlankCells = function() {
+	return this.reduce((acc, cell, index) => {
+			if(cell === 0) {
+				acc.push(BlankCell.create({
+					index,
+					possibleValues: this.getRelatedCells(index)
+						.getMissingDigits()
+				})[1])
+			}
+			
+			return acc
+		}, [])
+		.sort((x, y) => x.possibleValues.length - y.possibleValues.length)
+}
+
+Uint8Array.prototype.toSudokuFile = function() {
+	return Array.from(blankSudokuFile)
+		.map((x, i) => {
+			if(fileToStringConversionIndexes.includes(i)) {
+				return this[fileToStringConversionIndexes.indexOf(i)]
+			}
+			return x
+		})
+		.map(x => {
+			if(x === 0) return '_'
+			return String(x)
+		})
+		.join('')
+}
+
+const fileToStringConversionIndexes = Uint8Array.from([ 16, 17, 18, 20, 21, 22, 24, 25, 26, 30, 31, 32, 34,
+    35, 36, 38, 39, 40, 44, 45, 46, 48, 49, 50, 52, 53, 54, 72, 73, 74, 76, 77, 78, 80, 81, 82,
+    86, 87, 88, 90, 91, 92, 94, 95, 96, 100, 101, 102, 104, 105, 106, 108, 109, 110, 128, 129,
+    130, 132, 133, 134, 136, 137, 138, 142, 143, 144, 146, 147, 148, 150, 151, 152, 156, 157,
+    158, 160, 161, 162, 164, 165, 166 ])
+
+const sudokuFileValues = [ "_", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", "\n", "|", "_",
+		"-", "a", "b", "c", "d", "e", "f", "g", "h", "i" ]
+
+const groups = [
+    // Rows
+    [ 0, 1, 2, 3, 4, 5, 6, 7, 8 ],
+    [ 9, 10, 11, 12, 13, 14, 15, 16, 17 ],
+    [ 18, 19, 20, 21, 22, 23, 24, 25, 26 ],
+    [ 27, 28, 29, 30, 31, 32, 33, 34, 35 ],
+    [ 36, 37, 38, 39, 40, 41, 42, 43, 44 ],
+    [ 45, 46, 47, 48, 49, 50, 51, 52, 53 ],
+    [ 54, 55, 56, 57, 58, 59, 60, 61, 62 ],
+    [ 63, 64, 65, 66, 67, 68, 69, 70, 71 ],
+    [ 72, 73, 74, 75, 76, 77, 78, 79, 80 ],
+    // Columns
+    [ 0, 9, 18, 27, 36, 45, 54, 63, 72 ],
+    [ 1, 10, 19, 28, 37, 46, 55, 64, 73 ],
+    [ 2, 11, 20, 29, 38, 47, 56, 65, 74 ],
+    [ 3, 12, 21, 30, 39, 48, 57, 66, 75 ],
+    [ 4, 13, 22, 31, 40, 49, 58, 67, 76 ],
+    [ 5, 14, 23, 32, 41, 50, 59, 68, 77 ],
+    [ 6, 15, 24, 33, 42, 51, 60, 69, 78 ],
+    [ 7, 16, 25, 34, 43, 52, 61, 70, 79 ],
+    [ 8, 17, 26, 35, 44, 53, 62, 71, 80 ],
+    // Boxes
+    [ 0, 1, 2, 9, 10, 11, 18, 19, 20 ],
+    [ 3, 4, 5, 12, 13, 14, 21, 22, 23 ],
+    [ 6, 7, 8, 15, 16, 17, 24, 25, 26 ],
+    [ 27, 28, 29, 36, 37, 38, 45, 46, 47 ],
+    [ 30, 31, 32, 39, 40, 41, 48, 49, 50 ],
+    [ 33, 34, 35, 42, 43, 44, 51, 52, 53 ],
+    [ 54, 55, 56, 63, 64, 65, 72, 73, 74 ],
+    [ 57, 58, 59, 66, 67, 68, 75, 76, 77 ],
+    [ 60, 61, 62, 69, 70, 71, 78, 79, 80 ]
+]
+
+const blankSudokuFile = [ " ", " ", "a", "b", "c", " ", "d", "e", "f", " ", "g", "h", "i", "\n",
+	"1", " ", "_", "_", "_", "|", "_", "_", "_", "|", "_", "_", "_", "\n", "2", " ", "_", "_",
+	"_", "|", "_", "_", "_", "|", "_", "_", "_", "\n", "3", " ", "_", "_", "_", "|", "_", "_",
+	"_", "|", "_", "_", "_", "\n", " ", " ", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-",
+	"-", "\n", "4", " ", "_", "_", "_", "|", "_", "_", "_", "|", "_", "_", "_", "\n", "5", " ",
+	"_", "_", "_", "|", "_", "_", "_", "|", "_", "_", "_", "\n", "6", " ", "_", "_", "_", "|",
+	"_", "_", "_", "|", "_", "_", "_", "\n", " ", " ", "-", "-", "-", "-", "-", "-", "-", "-",
+	"-", "-", "-", "\n", "7", " ", "_", "_", "_", "|", "_", "_", "_", "|", "_", "_", "_", "\n",
+	"8", " ", "_", "_", "_", "|", "_", "_", "_", "|", "_", "_", "_", "\n", "9", " ", "_", "_",
+	"_", "|", "_", "_", "_", "|", "_", "_", "_" ]
--- test/Uint8Array.test.js
+++ test/Uint8Array.test.js
@@ -2,11 +2,31 @@ const { deepStrictEqual } = require('assert')
 require('../src/Uint8Array')
 
 describe('Array', () => {
+	const validFile = [
+		"  abc def ghi",
+		"1 7__|_4_|__1",
+		"2 __1|___|2__",
+		"3 _6_|2_9|_8_",
+		"  -----------",
+		"4 __3|5_4|9__",
+		"5 1__|___|__4",
+		"6 __2|1_8|5__",
+		"  -----------",
+		"7 _1_|9_6|_7_",
+		"8 __8|___|4__",
+		"9 6__|_2_|__8"
+	].join('\n')
+	const sudokuValues = Uint8Array.from([ 7, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 6, 0, 2, 0,
+		9, 0, 8, 0, 0, 0, 3, 5, 0, 4, 9, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 2, 1, 0, 8,
+		5, 0, 0, 0, 1, 0, 9, 0, 6, 0, 7, 0, 0, 0, 8, 0, 0, 0, 4, 0, 0, 6, 0, 0, 0, 2, 0, 0,
+		0, 8 ])
+		
 	describe('getMissingDigits', () => {
 		it('Should return missing digits from an array', () => {
 			deepStrictEqual(Uint8Array.from([ 1, 2, 3 ]).getMissingDigits(), Uint8Array.from([ 4, 5, 6, 7, 8, 9 ]))
 		})
 	})
+
 	describe('hasDuplicates', () => {
 		it('Should return true if an array has duplicates', () => {
 			deepStrictEqual(Uint8Array.from([ 1, 2, 2, 3 ]).hasDuplicates(), true)
@@ -15,6 +35,7 @@ describe('Array', () => {
 			deepStrictEqual(Uint8Array.from([ 1, 2, 3 ]).hasDuplicates(), false)
 		})
 	})
+
 	describe('replace', () => {
 		const originalArray = Uint8Array.from([ 1, 2, 3, 4, 5 ])
 		const newArray = originalArray.replace(1, 5)
@@ -28,9 +49,116 @@ describe('Array', () => {
 			deepStrictEqual(originalArray, Uint8Array.from([ 1, 2, 3, 4, 5 ]))
 		})
 	})
+
 	describe('unique', () => {
 		it('Should return only the unique values from an Array', () => {
 			deepStrictEqual(Uint8Array.from([ 1, 2, 2, 3 ]).unique(), Uint8Array.from([ 1, 2, 3]))
+		})
+	})
+
+	describe('fromSudokuFile', () => {
+		it('Should create a Uint8Array object from a .sudoku file string', () => {
+			deepStrictEqual(Uint8Array.fromSudokuFile(validFile)[1], sudokuValues)
+		})
+		it('Should provide an error if a string is not used', () => {
+			deepStrictEqual(Uint8Array.fromSudokuFile(5)[0], 'The \'sudokuFile\' parameter must be a string.')
+		})
+		it('Should provide an error if the sudokuString parameter is not 167 characters.', () => {
+			deepStrictEqual(Uint8Array.fromSudokuFile('hello')[0],
+				'The \'sudokuFile\' parameter must have a length of 167.')
+		})
+		it('Should identify illegal characters in an error.', () => {
+			const illegalCharacter = [
+	            "w abc def ghi",
+	            "1 7__|_4_|__1",
+	            "2 __1|___|2__",
+	            "3 _6_|2_9|_8_",
+	            "  -----------",
+	            "4 __3|5_4|9__",
+	            "5 1__|___|__4",
+	            "6 __2|1_8|5__",
+	            "  -----------",
+	            "7 _1_|9_6|_7_",
+	            "8 __8|___|4__",
+	            "9 6__|_2_|__8"
+	        ].join('\n')
+
+			deepStrictEqual(Uint8Array.fromSudokuFile(illegalCharacter)[0], 'You cannot use \'w\' in the \'sudokuFile\' parameter.')
+		})
+	})
+
+	describe('validateSudokuPuzzle', () => {
+		it('Should provide an error if 81 values are not provided.', () => {
+			deepStrictEqual(Uint8Array.from([ 1, 2 ]).validateSudokuPuzzle(), 'Sudoku puzzles must have a length of 81.')
+		})
+		it('Should provide an error if a value is below 0 or above 9.', () => {
+			const valuesWithTen = Uint8Array.from([ 10, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 6,
+				0, 2, 0, 9, 0, 8, 0, 0, 0, 3, 5, 0, 4, 9, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0,
+				2, 1, 0, 8, 5, 0, 0, 0, 1, 0, 9, 0, 6, 0, 7, 0, 0, 0, 8, 0, 0, 0, 4, 0, 0, 6, 0,
+				0, 0, 2, 0, 0, 0, 8 ])
+			
+			deepStrictEqual(Uint8Array.from(valuesWithTen).validateSudokuPuzzle(), 'Cells must be between 0 and 9, inclusive.')
+		})
+		it('Should identify an invalid puzzle', () => {
+			const invalidPuzzle = Uint8Array.from([ 7, 7, 0, 0, 4, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 6, 0,
+				2, 0, 9, 0, 8, 0, 0, 0, 3, 5, 0, 4, 9, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 2,
+				1, 0, 8, 5, 0, 0, 0, 1, 0, 9, 0, 6, 0, 7, 0, 0, 0, 8, 0, 0, 0, 4, 0, 0, 6, 0, 0,
+				0, 2, 0, 0, 0, 8 ])
+
+			deepStrictEqual(Uint8Array.from(invalidPuzzle).validateSudokuPuzzle(), 'The sudoku file is not valid because a group as two of the same value.')
+		})
+		it('Should provide an empty string if the puzzle is valid', () => {
+			deepStrictEqual(Uint8Array.from(sudokuValues).validateSudokuPuzzle(), '')
+		})
+	})
+
+	describe('getRelatedCells', () => {
+		it('Should return related cell values', () => {
+			const sudokuFile = [
+	            "  abc def ghi",
+	            "1 7__|_4_|__1",
+	            "2 __1|___|2__",
+	            "3 _6_|2_9|_8_",
+	            "  -----------",
+	            "4 __3|5_4|9__",
+	            "5 1__|___|__4",
+	            "6 __2|1_8|5__",
+	            "  -----------",
+	            "7 _1_|9_6|_7_",
+	            "8 __8|___|4__",
+	            "9 6__|_2_|__8"
+	        ].join("\n")
+	        const relatedToOne = Uint8Array.from([ 1, 4, 6, 7 ])
+
+	        deepStrictEqual(Uint8Array.fromSudokuFile(sudokuFile)[1].getRelatedCells(1), relatedToOne)
+		})
+	})
+
+	describe('getBlankCells', () => {
+		it('Should find the blank cells in a puzzle', () => {
+			const missingOne = [
+				"  abc def ghi",
+				"1 7_2|954|836",
+				"2 539|186|247",
+				"3 684|237|519",
+				"  -----------",
+				"4 325|479|681",
+				"5 198|365|724",
+				"6 476|821|953",
+				"  -----------",
+				"7 247|593|168",
+				"8 861|742|395",
+				"9 953|618|472"
+			].join('\n')
+
+			deepStrictEqual(Uint8Array.fromSudokuFile(missingOne)[1].getBlankCells(),
+				[ { index: 1, possibleValues: Uint8Array.from([ 1 ])} ])
+		})
+	})
+
+	describe('toSudokuFile', () => {
+		it('Should spit out a .sudoku file', () => {
+			deepStrictEqual(sudokuValues.toSudokuFile(), validFile)
 		})
 	})
 })
