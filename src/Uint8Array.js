59095b1fa830ae17cd426550844b81da5b675997
Max Tremaine <me@maxtremaine.com>
2023-12-08 00:08:48 -0500

    Moved from .from to .of where effective

--- src/Uint8Array.js
+++ src/Uint8Array.js
@@ -26,8 +26,9 @@ Uint8Array.fromSudokuFile = (sudokuFile) => {
 }
 
 Uint8Array.prototype.getMissingDigits = function() {
-	return Uint8Array.from([...Array(10).keys()].slice(1) // Range from 1 to 9, inclusive.
-		.filter(x => !this.includes(x)))
+	return Uint8Array.of(...Array(10).keys())
+		.slice(1) // Range from 1 to 9, inclusive.
+		.filter(x => !this.includes(x))
 }
 
 Uint8Array.prototype.hasDuplicates = function() {
@@ -109,11 +110,11 @@ Uint8Array.prototype.toSudokuFile = function() {
 		.join('')
 }
 
-const fileToStringConversionIndexes = Uint8Array.from([ 16, 17, 18, 20, 21, 22, 24, 25, 26, 30, 31, 32, 34,
+const fileToStringConversionIndexes = Uint8Array.of( 16, 17, 18, 20, 21, 22, 24, 25, 26, 30, 31, 32, 34,
     35, 36, 38, 39, 40, 44, 45, 46, 48, 49, 50, 52, 53, 54, 72, 73, 74, 76, 77, 78, 80, 81, 82,
     86, 87, 88, 90, 91, 92, 94, 95, 96, 100, 101, 102, 104, 105, 106, 108, 109, 110, 128, 129,
     130, 132, 133, 134, 136, 137, 138, 142, 143, 144, 146, 147, 148, 150, 151, 152, 156, 157,
-    158, 160, 161, 162, 164, 165, 166 ])
+    158, 160, 161, 162, 164, 165, 166 )
 
 const sudokuFileValues = [ "_", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", "\n", "|", "_",
 		"-", "a", "b", "c", "d", "e", "f", "g", "h", "i" ]
